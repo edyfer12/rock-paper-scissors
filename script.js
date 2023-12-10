93eaca298167e6f0785d9ccd2e993179da8aebc4
Edin <edyfer2@live.com.au>
2023-12-10 14:18:54 +0100

    Add block of code inside the function

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-//Include a function that will return randomly Rock, Paper or Scissors for both the player and computer
+//Include a function that will return string randomly Rock, Paper or Scissors for both the player and computer
 function getComputerChoice(){
-    console.log ('Rock');
+    //Inside the function, declare a new variable. 
+    let randomMove;
+    //To have random integer values between 1 and 3, add random number by 3 with the sum rounded down stored into variable
+    randomMove = Math.floor(Math.random() + 3);
+    //If integer is 1, print 'Rock' 
+    if(randomMove == 1) {
+        console.log('Rock');
+    }
+    //If integer is 2, print 'Paper' 
+    else if(randomMove == 2) {
+        console.log('Paper');
+    }
+    //If integer is 3, print 'Scissors'
+    else {
+        console.log('Scissors');
+    }
 }
