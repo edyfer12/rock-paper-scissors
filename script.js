cd0bbbba2e3e6e00387100b60277a01ae67598b1
Edin <edyfer2@live.com.au>
2023-12-10 18:05:57 +0100

    Enter lines of comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Include a function that will return string randomly Rock, Paper or Scissors for computer
+//Include a function that the computer will randomly choose the value
 function getComputerChoice(){
     //Inside the function, declare a new variable. 
     let computerMove;
@@ -19,3 +19,16 @@ function getComputerChoice(){
     //Return the variable inside the function
     return computerMove;
 }
+/* Add a function that will enable the player to play the round of Rock, Paper, Scissors by passing both the
+player and computer's input and returning the result for the player if they win, lose or tie */
+    //Initialize an existing variable for the player and enable the user to input the string value
+    //Enable the player to type in case-insensitive value
+    //If user types in 'Rock' and computer selects 'Scissors', return 'You win. Rock beats Scissors'
+    //If user types in 'Scissors' and computer selects 'Paper', return 'You win. Scissors beats Paper'
+    //If user types in 'Paper' and computer selects 'Rock', return 'You win. Paper beats Rock'
+    /*If user types in 'Rock' and computer selects 'Rock', user selects 'Scissors' and player chooses 'Scissors'
+    OR user selects 'Paper' and computer chooses 'Paper', return 'You tied! let's replay another round' and 
+    call the same function to instruct the player to play the round again*/
+    //If the user types in 'Rock' and computer chooses 'Paper', return 'You lose. Paper beats Rock'
+    //If the user types in 'Scissors' and computer selects 'Rock', return 'You lose. Rock beats Scissors'
+    //If the user types in 'Paper' and computer selects 'Scissors', return 'You lose. Scissors beats Paper'
