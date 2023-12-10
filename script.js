c0054f27f31e43776b4200a5c125be51dc79f179
Edin <edyfer2@live.com.au>
2023-12-10 14:31:37 +0100

    Change variable name

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //Include a function that will return string randomly Rock, Paper or Scissors for computer
 function getComputerChoice(){
     //Inside the function, declare a new variable. 
-    let randomMove;
+    let computerMove;
     //To have random integer values between 1 and 3, round down and multiply random number by 3 and store into variable
-    randomMove = Math.floor(Math.random() * 3) + 1;
+    computerMove = Math.floor(Math.random() * 3) + 1;
     //If integer is 1, print 'Rock' 
-    if(randomMove == 1) {
+    if(computerMove == 1) {
         console.log('Rock');
     }
     //If integer is 2, print 'Paper' 
-    else if(randomMove == 2) {
+    else if(computerMove == 2) {
         console.log('Paper');
     }
     //If integer is 3, print 'Scissors'
@@ -17,5 +17,5 @@ function getComputerChoice(){
         console.log('Scissors');
     }
     //Return the variable inside the function
-    return randomMove;
+    return computerMove;
 }
