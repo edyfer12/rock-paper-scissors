d6fdfabf55e9c5c770594541bd3a60457f6cd7f6
Edin <edyfer2@live.com.au>
2023-12-11 16:08:44 +0100

    Edit comments

--- script.js
+++ script.js
@@ -55,18 +55,18 @@ either from the player or computer */
     //Create a variable for the player and assign the input as a value, 'rock, paper, scissors' case insensitive
     /*Create a variable for the computer and assign the getComputerChoice function call to return random strings,
     'Rock', 'Paper' and 'Scissors'*/
-    //Invoke the playRound function() and 
+    //Invoke the playRound function() and print the invoked function to the console
     //Encourage the player to type in input stored into existing variable
     //Encourage the computer to return random strings and store into existing variable
-    //Invoke the playRound function() 
+    //Invoke the playRound function() and print the invoked function to the console
     //Encourage the player to type in input stored into existing variable
     //Encourage the computer to return random strings and store into existing variable
-    //Invoke the playRound function() 
+    //Invoke the playRound function() and print the invoked function to the console
     //Encourage the player to type in input stored into existing variable
     //Encourage the computer to return random strings and store into existing variable
-    //Invoke the playRound function() 
+    //Invoke the playRound function() and print the invoked function to the console
     //Encourage the player to type in input stored into existing variable
     //Encourage the computer to return random strings and store into existing variable
-    //Invoke the playRound function() 
+    //Invoke the playRound function() and print the invoked function to the console
     //If the player's score is 5, print to console 'Player wins the game' and set player's and computer's score to 0
     //If the computer's score is 5, print to console 'Computer wins the game' and set player's and computer's score to 0
