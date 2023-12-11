60e32e687ad58e77cb0f454f4db8dc9a130f8297
Edin <edyfer2@live.com.au>
2023-12-11 11:17:24 +0100

    Declare a function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //Declare function that computer makes a move
+function getComputerChoice() {
     //Declare a variable called computerMove
     //Assign the variable to a random number value, multiplied by 3 all rounded up
     //If variable is 1, return 'Rock'
     //If variable is 2, return 'Paper'
     //If variable is 3, return 'Scissors'
+}
