accc9b483d084eb42ef7cdb279faf555e98fca42
Edin <edyfer2@live.com.au>
2023-12-11 12:03:20 +0100

    Add comments

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ function getComputerChoice() {
         return 'Scissors';
     }
 }
+
+/* Declare a function that enables both the computer and player to play one round of the game 
+with two parameters passed, playerSelection and computerSelection */
+/*If the playerSelection is equal to 'Rock' and computerSelection is equal to 'Scissors', 
+playerSelection is equal to 'Paper' to computerSelection is equal to 'Rock' OR 
+playerSelection is equal to 'Scissors' and computerSelection is equal to 'Paper', 
+return 'You Win!'*/
+/*If the playerSelection is equal to 'Rock' and computerSelection is equal to 'Paper', 
+playerSelection is equal to 'Paper' to computerSelection is equal to 'Scissors' OR 
+playerSelection is equal to 'Scissors' and computerSelection is equal to 'Rock',
+return 'You Lose!'*/
+//If the playerSelection has the same string as the computerSelection, return 'Tie!'
