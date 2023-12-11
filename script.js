f5a30b92e3dbc076a38a0152ae9cd6f5f7eddb7d
Edin <edyfer2@live.com.au>
2023-12-11 16:47:44 +0100

    Edit the lines of code

--- script.js
+++ script.js
@@ -52,32 +52,33 @@ function playRound(playerSelection, computerSelection){
 either from the player or computer */
 function game(){
     //Create a variable for the player and assign the input as a value, 'rock, paper, scissors' case insensitive
-    let playerSelection = prompt('Rock, Paper, or Scissors').toLowerCase;
+    let playerSelection = prompt('Rock, Paper, or Scissors').toLowerCase();
     /*Create a variable for the computer and assign the getComputerChoice function call to return random strings,
     'Rock', 'Paper' and 'Scissors'*/
     let computerSelection = getComputerChoice();
+    console.log('computerSelection: '+ computerSelection);
     //Invoke the playRound function() and print the invoked function to the console
     console.log(playRound(playerSelection, computerSelection));
     //Encourage the player to type in input stored into existing variable
-    playerSelection = prompt('Rock, Paper, or Scissors').toLowerCase;
+    playerSelection = prompt('Rock, Paper, or Scissors').toLowerCase();
     //Encourage the computer to return random strings and store into existing variable
     computerSelection = getComputerChoice();
     //Invoke the playRound function() and print the invoked function to the console
     console.log(playRound(playerSelection, computerSelection));
     //Encourage the player to type in input stored into existing variable
-    playerSelection = prompt('Rock, Paper, or Scissors').toLowerCase;
+    playerSelection = prompt('Rock, Paper, or Scissors').toLowerCase();
     //Encourage the computer to return random strings and store into existing variable
     computerSelection = getComputerChoice();
     //Invoke the playRound function() and print the invoked function to the console
     console.log(playRound(playerSelection, computerSelection));
     //Encourage the player to type in input stored into existing variable
-    playerSelection = prompt('Rock, Paper, or Scissors').toLowerCase;
+    playerSelection = prompt('Rock, Paper, or Scissors').toLowerCase();
     //Encourage the computer to return random strings and store into existing variable
     computerSelection = getComputerChoice();
     //Invoke the playRound function() and print the invoked function to the console
     console.log(playRound(playerSelection, computerSelection));
     //Encourage the player to type in input stored into existing variable
-    playerSelection = prompt('Rock, Paper, or Scissors').toLowerCase;
+    playerSelection = prompt('Rock, Paper, or Scissors').toLowerCase();
     //Encourage the computer to return random strings and store into existing variable
     computerSelection = getComputerChoice();
     //Invoke the playRound function() and print the invoked function to the console
