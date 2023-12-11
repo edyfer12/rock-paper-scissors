30cf221381548357910b2bdd0508df5337993e65
Edin <edyfer2@live.com.au>
2023-12-11 22:12:41 +0100

    Modify if statements

--- script.js
+++ script.js
@@ -126,11 +126,11 @@ function game(){
         computerScore++;
     }
     //If the player scores five points, print to console, 'Player Won the Game!' and set both computer and player score to 0
-    if(playerScore == 5){
+    if(playerScore === 5){
         console.log(`Player Won the Game! (${playerScore} : ${computerScore})`);
     }
     //If the computer scores five points, print to console, 'Player Lost the Game!' and set both computer and player score to 0
-    if(computerScore == 5){
+    else if(computerScore === 5){
         console.log(`Computer Won the Game! (${computerScore} : ${playerScore})`);
     } 
 }
