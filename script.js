893a97090925e37b434e0db691a6636d83a804fb
Edin <edyfer2@live.com.au>
2023-12-11 11:18:42 +0100

    Assign random integer to the variable

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function getComputerChoice() {
     //Declare a variable called computerMove
     let computerMove;
     //Assign the variable to a random number value, multiplied by 3 all rounded up
+    computerMove = Math.ceil(Math.random() * 3);
     //If variable is 1, return 'Rock'
     //If variable is 2, return 'Paper'
     //If variable is 3, return 'Scissors'
