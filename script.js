aff290ec9cd4fffe50030afbf8dbfbc54fc657ea
Edin <edyfer2@live.com.au>
2023-12-11 11:16:26 +0100

    Create lines of comments

---
+++ script.js
@@ -0,0 +1,6 @@
+//Declare function that computer makes a move
+    //Declare a variable called computerMove
+    //Assign the variable to a random number value, multiplied by 3 all rounded up
+    //If variable is 1, return 'Rock'
+    //If variable is 2, return 'Paper'
+    //If variable is 3, return 'Scissors'
