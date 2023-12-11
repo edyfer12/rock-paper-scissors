c0b1351195cb5de2009f8974b053fb25451f94d8
Edin <edyfer2@live.com.au>
2023-12-11 12:33:22 +0100

    Edit the expression on if statements

--- script.js
+++ script.js
@@ -25,18 +25,18 @@ function playRound(playerSelection, computerSelection){
     playerSelection is equal to 'Paper' to computerSelection is equal to 'Rock' OR 
     playerSelection is equal to 'Scissors' and computerSelection is equal to 'Paper', 
     return 'You Win!'*/
-    if(playerSelection === 'Rock' && computerSelection === 'Scissors' || 
-    playerSelection === 'Paper' && computerSelection === 'Rock' ||
-    playerSelection === 'Scissors' && computerSelection === 'Paper'){
+    if(playerSelection === 'rock' && computerSelection === 'Scissors' || 
+    playerSelection === 'paper' && computerSelection === 'Rock' ||
+    playerSelection === 'scissors' && computerSelection === 'Paper'){
         return 'You Win!';
     }
     /*If the playerSelection is equal to 'Rock' and computerSelection is equal to 'Paper', 
     playerSelection is equal to 'Paper' to computerSelection is equal to 'Scissors' OR 
     playerSelection is equal to 'Scissors' and computerSelection is equal to 'Rock',
     return 'You Lose!'*/
-    else if (playerSelection === 'Rock' && computerSelection === 'Paper' ||
-    playerSelection === 'Paper' && computerSelection === 'Scissors' ||
-    playerSelection === 'Scissors' && computerSelection === 'Rock') {
+    else if (playerSelection === 'rock' && computerSelection === 'Paper' ||
+    playerSelection === 'paper' && computerSelection === 'Scissors' ||
+    playerSelection === 'scissors' && computerSelection === 'Rock') {
         return 'You lose';
     }
     //If the playerSelection has the same string as the computerSelection, return 'Tie!'
