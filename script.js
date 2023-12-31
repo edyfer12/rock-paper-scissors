578b266a234f7dd05299c262f8f366988cd736b0
Edin <edyfer2@live.com.au>
2023-12-31 23:46:25 +0100

    Edit codes

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function startGame(){
 
         //If the player or computer score 5 points, end the game
         if(playerScore === 5 || computerScore === 5){
-            endGame();
+            endGame(playerScore,computerScore);
         }
     });
 
@@ -135,8 +135,8 @@ function startGame(){
         }
 
         //If the player or computer score 5 points, end the game
-        if(playerScore === 5 || playerScore === 5){
-            endGame();
+        if(playerScore === 5 || computerScore === 5){
+            endGame(playerScore,computerScore);
         }
     });
 
@@ -160,66 +160,10 @@ function startGame(){
 
         //If the player or computer score 5 points, notify the player as the winner and end the game
         if(playerScore === 5 || computerScore === 5){
-            endGame();
+            endGame(playerScore, computerScore);
         }
     });
-    
-    
-
     //*************************************************************************************************** */
-
-    /*//When the user clicks on the button named Rock, invoke the play round function
-    rock.addEventListener('click', playRound);
-    //When the user clicks on the button named Paper, invoke the play round function
-    paper.addEventListener('click', playRound);
-    //When the user clicks on the button named Scissors, invoke the play round function
-    scissors.addEventListener('click', playRound);
-
-    //Return the reference to the div element with results class
-    let result = document.querySelector('.result'); 
-    //Create a paragraph element for id being player using JS and store into variable created
-    let paraPlayer = document.createElement('p');
-    paraPlayer.setAttribute('id', 'playerScore');
-    //Create a paragraph element for id being computer using JS and store into variable created
-    let paraComputer = document.createElement('p');
-    paraComputer.setAttribute('id', 'computerScore');
-
-    //If the player wins, 
-    if(playRound(playerSelection, computerSelection) === 'You Win!'){
-        //Increment the player score by 1 in prefix form
-        ++playerScore;
-        //Set the text content for paragraph where id is player to player score incremented by 1
-        paraPlayer.textContent = playerScore;
-        //Append the paragraph element where id being player to the results div element
-        result.appendChild(paraPlayer);
-    }
-    //If the computer wins,
-    else if(playRound(playerSelection, computerSelection) === 'You lose!'){
-        //Increment the computer score by 1 in prefix form
-        ++computerScore;
-        //Set the text content for paragraph where id is computer to computer score incremented by 1
-        paraComputer.textContent = computerScore;
-        //Append the paragraph element where id being computer to the results div element
-        result.appendChild(paraComputer);
-    }
-    //If the player scores more than the computer, print to console, 'Player Won the Game!' and set both computer and player score to 0
-    if(playerScore > computerScore){
-        console.log(`Player Won the Game! (${playerScore} : ${computerScore})`);
-        playerScore = 0;
-        computerScore = 0;
-    }
-    //If the computer scores more than the player, print to console, 'Computer Won the Game!' and set both computer and player score to 0
-    else if(computerScore > playerScore){
-        console.log(`Computer Won the Game! (${computerScore} : ${playerScore})`);
-        playerScore = 0;
-        computerScore = 0;
-    } 
-    //If the player scores equally to the player, print to console, 'The game is tied!' and set both scores to 0
-    else {
-        console.log('The game is tied!');
-        playerScore = 0;
-        computerScore = 0;
-    }*/
 }
 
 //Declare score variables for player and computer and set both to 0
