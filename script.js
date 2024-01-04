407f59fde43f76a33e61326430b7c7692a3bb27d
Edin <edyfer2@live.com.au>
2024-01-04 20:44:25 +0100

    Remove block of code and add comment

--- script.js
+++ script.js
@@ -100,14 +100,14 @@ function startGame(){
         let computerSelection = getComputerChoice();
 
         //If the score from the player or computer is greater than 5, reset the game
-        if(playerScore >= 5 || computerScore >= 5){
+       /* if(playerScore >= 5 || computerScore >= 5){
             //Reset both scores to 0
             computerScore = 0;
             playerScore = 0;
             //Display the scores for player and computer
             paragraphComputer.textContent = 'Computer Score: ' + computerScore;
             paragraphPlayer.textContent = 'Player Score: ' + playerScore;
-        }
+        }*/
        
         //If the player wins the round, add player score by 1 and display the added score for player
         if(playRound(playerSelection,computerSelection) === 'You Win!'){
@@ -128,6 +128,7 @@ function startGame(){
             else if(computerScore === 5) {
                 paragraphAnnounce.textContent = 'Computer Wins the Game!';
             }
+            //If the user clicks on the button again, display the scores for both player and computer as zero
         }
     });
 
