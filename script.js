9fde45b4cc46cbaaaf8642c661fe791bb1ecdf6e
Edin <edyfer2@live.com.au>
2024-01-05 14:06:46 +0100

    Edit code

--- script.js
+++ script.js
@@ -111,12 +111,12 @@ function startGame(){
         //If the player wins the round, add player score by 1, display the added score for player and announcement
         if(playRound(playerSelection,computerSelection) === 'You Win!'){
             paragraphPlayer.textContent = 'Player Score: ' + ++playerScore;
-            paragraphAnnounce.textContent = 'You win this round!';
+            paragraphAnnounce.textContent = 'You won this round!';
         }
         //If the player loses the round, add computer score by 1, display the added score for computer and announcement
         else if(playRound(playerSelection, computerSelection) === 'You lose!'){
             paragraphComputer.textContent = 'Computer Score: ' + ++computerScore;
-            paragraphAnnounce.textContent = 'You lose this round!';
+            paragraphAnnounce.textContent = 'You lost this round!';
         }
         //If the player ties the round, just display the announcement and do not increment the scores 
         else {
